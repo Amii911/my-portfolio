2fdda56e65b26c7940775b1cd3b2180af140ec82
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-12-19 16:43:07 -0500

    adding assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-    Hello!
-    </>
+    <div>
+
+    </div>
   )
 }
 
