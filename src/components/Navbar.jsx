81e4257aabf88a9475459ff5572a71f1615985ed
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-12-20 13:27:16 -0500

    nav and styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React from 'react';
+import {FaBars, FaTimes} from 'react-icons/fa';
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#CFDCD1] text-[#1f2435] font-bold'>
+      <div>Aminat Adebayo</div>
+      {/* menu */}
+      <div>
+        <ul className='flex'>
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Project</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      {/* Hamburger */}
+      <div className='hidden'>
+        <FaBars/>
+      </div>
+
+      {/* Mobile menu */}
+      <ul className='hidden'>
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Project</li>
+          <li>Contact</li>
+      </ul>
+
+      {/* social icons */}
+      <div></div>
+    </div>
+
   )
 }
 
