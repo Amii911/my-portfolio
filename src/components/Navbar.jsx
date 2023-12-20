88972b4a835f3232cb1ed96c1fdbda4436d76c16
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-12-20 13:42:47 -0500

    nav and styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {FaBars, FaTimes} from 'react-icons/fa';
 
 function Navbar() {
+  const [nav, setNav] = useState(false)
+  const handleClick = () => setNav(!nav)
+
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#CFDCD1] text-[#1f2435] font-bold'>
-      <div>Aminat Adebayo</div>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#CFDCD1] text-[#1f2435]'>
+      <div className='text-2xl font-bold'>Aminat Adebayo</div>
       {/* menu */}
-      <div>
-        <ul className='flex'>
+        <ul className='hidden md:flex'>
           <li>Home</li>
           <li>About</li>
           <li>Skills</li>
           <li>Project</li>
           <li>Contact</li>
         </ul>
-      </div>
+      
       {/* Hamburger */}
-      <div className='hidden'>
+      <div className='md:hidden z-10'>
         <FaBars/>
       </div>
 
       {/* Mobile menu */}
-      <ul className='hidden'>
-          <li>Home</li>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Project</li>
-          <li>Contact</li>
+      <ul className='hidden absolute top-0 left-0 w-full h-screen bg-[#CFDCD1] flex flex-col justify-center items-center'>
+          <li className='py-6 text-3xl'>Home</li>
+          <li className='py-6 text-3xl'>About</li>
+          <li className='py-6 text-3xl'>Skills</li>
+          <li className='py-6 text-3xl'>Project</li>
+          <li className='py-6 text-3xl'>Contact</li>
       </ul>
 
       {/* social icons */}
-      <div></div>
+      <div className='hidden'></div>
     </div>
 
   )
