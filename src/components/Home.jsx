4c5fc32a0f21971f778ce6affe49ec9b5f02aee1
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-12-22 20:43:27 -0500

    home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
+import { TypeAnimation } from 'react-type-animation';
 // import {HiArrowNarrowRight} from 'react-icons/hi';
 
 function Home() {
   return (
-    <div className='w-full h-screen'>
-      
+    <div name='home' className='w-full h-screen'>
+
         {/* Container */}
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-            <p>Hello! I'm Aminat - I build things and enjoy creating user friendly applications.</p>
+            <p>Welcome!</p>
+            <h1>I'm Aminat Adebayo | Brooklyn, NY</h1>
+            <h2> <TypeAnimation
+      sequence={[
+        'FULL-STACK SOFTWARE ENGINEER',
+        1000,
+        'CODER',
+        1000,
+        'TECH ENTHUSIAST',
+        1000
+      ]}
+      wrapper="span"
+      speed={50}
+      style={{ fontSize: '1em', display: 'inline-block' }}
+      repeat={Infinity}
+    /></h2>
         </div>
     </div>
   )
