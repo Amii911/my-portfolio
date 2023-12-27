15256274b8cd28385b8a6c62ce8adbe9a7a0e3d7
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-12-26 21:36:57 -0500

    projects

---
+++ src/components/Projects.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div name='projects' className='w-full md:h-screen text-[#1f2435] bg-[#f1ecea]'>
+        <div>
+            <div>
+                <p>Projects</p>
+                <p>check out my projects</p>
+            </div>
+            <div>
+                <div>
+                    <div>
+                        <span>
+
+                        </span>
+                        <div>
+                            <a href='/'>
+                                <button></button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Projects
