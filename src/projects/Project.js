3e57be6c96c9fc3a91f354de4c32c5a4a45918c4
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-12-26 21:51:42 -0500

    projects

---
+++ src/projects/Project.js
@@ -0,0 +1,37 @@
+import Blog from '../assets/Blog.png';
+import RRNYC from '../assets/RRNYC.png';
+import TransXceed from '../assets/TransXceed.jpg';
+
+
+export const data=[
+    {
+        id:1,
+        name:"Restroom Radar NYC",
+        image:RRNYC,
+        github:"https://github.com/Amii911/Restroom-Radar-NYC",
+        live:"https://youtu.be/IGvnnkiywwo",
+    },
+    {
+        id:2,
+        name:"TransXceed",
+        image:TransXceed,
+        github:"https://github.com/Amii911/TransXceed",
+        live:"https://youtu.be/a6B-StFFslU",
+    },
+    {
+        id:3,
+        name:"Bloggin BLog",
+        image:Blog,
+        github:"https://github.com/Amii911/bloggin-blog",
+        live:"https://youtu.be/2LukRw3nzuI",
+    },
+    // {
+    //     id:4,
+    //     name:"React JS Application",
+    //     image:RealEst,
+    //     github:"",
+    //     live:"",
+    // },
+
+
+]
