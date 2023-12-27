30d008ac9aa9280d1fa3a8b686b8fead55e54bba
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-12-26 23:15:52 -0500

    contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,4 @@
 import React, {useState} from 'react';
-import {FaGithub, FaLinkedin, FaMediumM} from 'react-icons/fa'
-
 
 function Contact() {
     const [fromName, setFromName] = useState('')
@@ -33,40 +31,19 @@ function Contact() {
     }
   
     return (
-      <div className="email-form-container">
-        {/* <div className="email-form">
-          <form onSubmit={handleSubmit}>
-            <label>
-              Your Name:
-              <input
-                type="text"
-                value={fromName}
-                onChange={(e) => setFromName(e.target.value)}
-                required
-              />
-            </label>
-            <label>
-              Your Email:
-              <input
-                type="email"
-                value={fromEmail}
-                onChange={(e) => setFromEmail(e.target.value)}
-                required
-              />
-            </label>
-            <label>
-              Message:
-              <textarea
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-                required
-              />
-            </label>
-            <button type="submit">Send Email</button>
+      <div name='contact' className='w-full h-screen text-[#1f2435] bg-[#f1ecea] flex justify-center items-center p-4'>
+          <form onSumbit={handleSubmit} className='flex flex-col max-w-[600px] w-full'>
+              <div className='pb-8'>
+                  <p className='text-4xl font-bold inline border-b-4 text-[#1f2435]'>Contact</p>
+                  <p className='text-[#1f2435] py-4'>Submit the form below or email me - adebayo.aminat@gmail.com</p>
+              </div>
+              <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
+              <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
+              <textarea className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message'></textarea>
+              <button className='text-[#1f2435] border-2 hover:bg-[#8892b0] hover:border-[#8892b0] px-4 py-3 my-8 mx-auto flex items-center'>Send</button>
           </form>
-        </div> */}
       </div>
     )
-}
+  }
 
 export default Contact
