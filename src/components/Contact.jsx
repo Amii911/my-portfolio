e5f9a48c4b1fcb205be795af42f5d64878f0c621
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-12-26 23:33:47 -0500

    contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import emailjs from '@emailjs/browser';
 
 function Contact() {
     const [fromName, setFromName] = useState('')
@@ -32,14 +33,14 @@ function Contact() {
   
     return (
       <div name='contact' className='w-full h-screen text-[#1f2435] bg-[#f1ecea] flex justify-center items-center p-4'>
-          <form onSumbit={handleSubmit} className='flex flex-col max-w-[600px] w-full'>
+          <form onSubmit={handleSubmit} className='flex flex-col max-w-[600px] w-full'>
               <div className='pb-8'>
                   <p className='text-4xl font-bold inline border-b-4 text-[#1f2435]'>Contact</p>
                   <p className='text-[#1f2435] py-4'>Submit the form below or email me - adebayo.aminat@gmail.com</p>
               </div>
-              <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
-              <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
-              <textarea className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message'></textarea>
+              <input className='p-2' type="text" placeholder='Name' name='name' value={fromName} onChange={(e) => setFromName(e.target.value)}/>
+              <input className='my-4 p-2' type="email" placeholder='Email' name='email' value={fromEmail} onChange={(e)=> setFromEmail(e.target.value)} />
+              <textarea className='p-2' name="message" rows="10" placeholder='Message' value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
               <button className='text-[#1f2435] border-2 hover:bg-[#8892b0] hover:border-[#8892b0] px-4 py-3 my-8 mx-auto flex items-center'>Send</button>
           </form>
       </div>
